bfe6d689e0d00923346e8eda2289f08bf50d7994
IdealistCat <mattallan.h@gmail.com>
2024-11-17 06:29:01 -0600

    fix outdated state link

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -44,7 +44,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/Psych-Slice/P-Slice/releases");
+				CoolUtil.browserLoad("https://github.com/sphis-Sinco/vsGuyPlus/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
