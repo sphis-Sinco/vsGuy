973aadf43c5edc8958c4342415c5ec6901fea5cc
IdealistCat <mattallan.h@gmail.com>
2024-11-17 07:16:33 -0600

    trycatch statement to hopefully fix crash I got

--- source/cutscenes/DialogueCharacter.hx
+++ source/cutscenes/DialogueCharacter.hx
@@ -51,26 +51,30 @@ class DialogueCharacter extends FlxSprite
 	}
 
 	public function reloadCharacterJson(character:String) {
-		var characterPath:String = 'images/dialogue/' + character + '.json';
-		var rawJson = null;
+		try {
+			var characterPath:String = 'images/dialogue/' + character + '.json';
+			var rawJson = null;
 
-		#if MODS_ALLOWED
-		var path:String = Paths.modFolders(characterPath);
-		if (!FileSystem.exists(path)) {
-			path = Paths.getSharedPath(characterPath);
+			#if MODS_ALLOWED
+			var path:String = Paths.modFolders(characterPath);
+			if (!FileSystem.exists(path)) {
+				path = Paths.getSharedPath(characterPath);
+			}
+
+			if(!FileSystem.exists(path)) {
+				path = Paths.getSharedPath('images/dialogue/' + DEFAULT_CHARACTER + '.json');
+			}
+			rawJson = File.getContent(path);
+
+			#else
+			var path:String = Paths.getSharedPath(characterPath);
+			rawJson = Assets.getText(path);
+			#end
+			
+			jsonFile = cast Json.parse(rawJson);
+		} catch(e) {
+			trace(e);
 		}
-
-		if(!FileSystem.exists(path)) {
-			path = Paths.getSharedPath('images/dialogue/' + DEFAULT_CHARACTER + '.json');
-		}
-		rawJson = File.getContent(path);
-
-		#else
-		var path:String = Paths.getSharedPath(characterPath);
-		rawJson = Assets.getText(path);
-		#end
-		
-		jsonFile = cast Json.parse(rawJson);
 	}
 
 	public function reloadAnimations() {
