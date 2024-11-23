6a47b68ab19ace83cce5682594356a9485e57860
IdealistCat <mattallan.h@gmail.com>
2024-11-22 22:20:36 -0600

    checks

--- source/sinco/vsguy/modding/DLC.hx
+++ source/sinco/vsguy/modding/DLC.hx
@@ -14,9 +14,10 @@ class DLC
 	public static var DLC_FOLDER:String = './dlcs';
     public static var DLC_APIVER:String = '0.0.1';
 
-	public static function updateDLCList()
+	public static function updateDLCList(?checks:Int = 2)
 	{
 		var newlist:Array<String> = [];
+        var loops:Int = 0;
 
 		#if desktop
 		var dlc_folders:Array<String> = FileSystem.readDirectory('$DLC_FOLDER/');
@@ -32,25 +33,28 @@ class DLC
         // trace(dlc_folders);
         // dlc_folders.push('');
 		
-        for (i in 0...dlc_folders.length)
-		{
-            var folder = dlc_folders[i];
-			var path:String = '$DLC_FOLDER/$folder/';
+        while (loops < checks){
+            for (i in 0...dlc_folders.length)
+            {
+                var folder = dlc_folders[i];
+                var path:String = '$DLC_FOLDER/$folder/';
 
-			try {
-                var dir = FileSystem.readDirectory(path);
-                
-                var dlcfile:DLCMeta = Json.parse(Assets.getText(path+'dlc.json'));
+                try {
+                    var dir = FileSystem.readDirectory(path);
+                    
+                    var dlcfile:DLCMeta = Json.parse(Assets.getText(path+'dlc.json'));
 
-                if (dlcfile.api_version != DLC_APIVER) {
+                    if (dlcfile.api_version != DLC_APIVER) {
+                        dlc_folders.remove(folder);
+                    }
+                } catch(e)
+                {
+                    trace(e);
                     dlc_folders.remove(folder);
                 }
-            } catch(e)
-            {
-                trace(e);
-				dlc_folders.remove(folder);
             }
-		}
+            loops++;
+        }
         
         // trace(dlc_folders);
 
