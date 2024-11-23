e7d258ce415a22a7c0bc562b5abe146e9d749720
IdealistCat <mattallan.h@gmail.com>
2024-11-23 14:12:53 -0600

    Revert "arg"
    This reverts commit 9c0e7352fd03ddca233c735f0d42b12a03f1bc04.

--- source/substates/StickerSubState.hx
+++ source/substates/StickerSubState.hx
@@ -1,8 +1,27 @@
 package substates;
 
+import mikolka.compatibility.VsliceOptions;
 import states.MainMenuState;
+import flixel.FlxSprite;
+import haxe.Json;
+import lime.utils.Assets;
+// import flxtyped group
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxTimer;
 import flixel.FlxG;
+import flixel.math.FlxMath;
+import flixel.util.FlxSort;
+import flixel.addons.transition.FlxTransitionableState;
+import openfl.display.BitmapData;
+import openfl.geom.Matrix;
+import openfl.display.Sprite;
+import openfl.display.Bitmap;
 import flixel.FlxState;
+
+using Lambda;
+using mikolka.funkin.IteratorTools;
+using StringTools;
+using mikolka.funkin.utils.ArrayTools;
 
 class StickerSubState extends MusicBeatSubstate
 {
