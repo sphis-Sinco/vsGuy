8203f69c258c9d7bafa444088cd93fca29470360
IdealistCat <mattallan.h@gmail.com>
2024-11-22 18:16:37 -0600

    null object reference fix

--- source/objects/MenuBG.hx
+++ source/objects/MenuBG.hx
@@ -4,16 +4,16 @@ class MenuBG extends FlxSprite
 {
     override public function new(bg:String = 'menuBG', ?yScroll:Float = 0)
     {
+        super(x,y);
+
         loadGraphic(Paths.image('menuBG'));
 
         antialiasing = ClientPrefs.data.antialiasing;
 		scrollFactor.set(0, yScroll);
 		setGraphicSize(Std.int(width * 1.175));
 		updateHitbox();
-		screenCenter();
 
         setPosition(-80);
-
-        super(x,y);    
+		screenCenter();  
     }
 }
