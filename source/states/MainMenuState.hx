891629ba8866430ba5208f83ccbf75a92f536e6e
IdealistCat <mattallan.h@gmail.com>
2024-11-22 18:19:36 -0600

    use the file

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -86,8 +86,7 @@ class MainMenuState extends MusicBeatState
 		camFollow = new FlxObject(0, 0, 1, 1);
 		add(camFollow);
 
-		magenta = new MenuBG('menuDesat', yScroll);
-		magenta.color = 0xFFfd719b;
+		magenta = new MenuBG('menuBGMagenta', yScroll);
 		magenta.visible = false;
 		add(magenta);
 
