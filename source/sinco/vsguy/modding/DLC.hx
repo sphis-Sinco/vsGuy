137d7143e5daa7d12f3842a70c2f84a133625fbc
IdealistCat <mattallan.h@gmail.com>
2024-11-22 21:27:18 -0600

    second wipe for folders without any files

--- source/sinco/vsguy/modding/DLC.hx
+++ source/sinco/vsguy/modding/DLC.hx
@@ -5,12 +5,15 @@ class DLC
 
     public static var dlcs:Array<String> = [];
 
+    public static var DLC_FOLDER:String = './dlcs';
+
     public static function updateDLCList()
     {
         var newlist:Array<String> = [];
 
         #if desktop
-        var dlc_folders:Array<String> = FileSystem.readDirectory('./dlcs/');
+        var dlc_folders:Array<String> = FileSystem.readDirectory('$DLC_FOLDER/');
+
         for (file in dlc_folders)
         {
             if (file.contains('.') || file == 'readme.text')
@@ -19,7 +22,24 @@ class DLC
                 trace('Removed file from dlc_folders: $file');
             }
         }
+
+        for (folder in dlc_folders)
+        {
+            var isfolder:Bool = false;
+            try {
+                isfolder = FileSystem.isDirectory('$DLC_FOLDER/$folder/');
+            } catch(e) {}
+
+            if (isfolder)
+                trace('$folder is a folder (with something in it)!');
+            else {
+                trace('$folder is not a folder (with anything in it)!');
+                dlc_folders.remove(folder);
+            }
+        }
+
         trace('DLC Folder Content: $dlc_folders');
+
         for (folder in dlc_folders)
         {
             trace('Added folder to newlist $folder');
