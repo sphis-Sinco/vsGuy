b095cee68fd8b178f5875e6ff32094a94d9d4248
IdealistCat <mattallan.h@gmail.com>
2024-11-22 20:31:50 -0600

    selector state for dlc testing

---
+++ source/sinco/vsguy/testing/dlc/Selector.hx
@@ -0,0 +1,14 @@
+package sinco.vsguy.testing.dlc;
+
+class Selector extends MusicBeatState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
