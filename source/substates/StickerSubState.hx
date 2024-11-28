375dc868b5e57a5c69e4af3071cef752df300489
IdealistCat <mattallan.h@gmail.com>
2024-11-28 17:20:54 -0600

    maybe there is a problem with the stickers

--- source/substates/StickerSubState.hx
+++ source/substates/StickerSubState.hx
@@ -159,7 +159,7 @@ class StickerSubState extends MusicBeatSubstate
 
     trace("Collecting stickers...");
     trace("Current mod: "+Mods.currentModDirectory);
-    var stickers:StickerInfo = null;
+    var stickers:Null<StickerInfo> = null;
 
     // var globalMods = Mods.getGlobalMods().map(s -> "mods/"+s);
     // globalMods.pushUnique("mods/"+Mods.currentModDirectory);
