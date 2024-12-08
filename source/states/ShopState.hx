ae1e6c97087967676f8a656fce1bb087f89a95a7
IdealistCat <mattallan.h@gmail.com>
2024-12-07 21:26:03 -0600

    item description textField

--- source/states/ShopState.hx
+++ source/states/ShopState.hx
@@ -7,7 +7,8 @@ class ShopState extends MusicBeatState
 	public var sinco:Character;
 	public var card:FlxSprite;
 
-    public var itemName:FlxText;
+	public var itemName:FlxText;
+	public var itemDesc:FlxText;
 
 	override public function new()
 	{
@@ -18,13 +19,17 @@ class ShopState extends MusicBeatState
 
 		card = new FlxSprite(0, 0).loadGraphic(Paths.image('shop/Item Card'));
 		card.screenCenter();
-        card.x += 380;
+		card.x += 380;
 
-		itemName = new FlxText(0,0,0,"Item", 16);
+		itemName = new FlxText(0, 0, 0, "Item - $Price", 16);
 		itemName.setFormat(Paths.font("comicsans.ttf"), 64, FlxColor.RED, LEFT);
 		itemName.screenCenter();
-		itemName.x += 160;
-		itemName.y -= 320;
+		itemName.x += 280;
+		itemName.y -= 300;
+
+        itemDesc = new FlxText(0,0,0, "Description", 16);
+        itemDesc.setFormat(Paths.font("comicsans.ttf"), 32, itemName.color, itemName.alignment);
+		itemDesc.setPosition(itemName.x + 20, itemName.y + 100);
 
 		super();
 	}
@@ -36,6 +41,7 @@ class ShopState extends MusicBeatState
         // FlxTween.color(card, 1, card.color, FlxColor.RED);
 
 		add(itemName);
+		add(itemDesc);
 
 		super.create();
 	}
