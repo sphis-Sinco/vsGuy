2f573930f43ee34164acd66788da305c6ad00e10
IdealistCat <mattallan.h@gmail.com>
2024-12-08 19:14:59 -0600

    pause text!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- source/sinco/vsguy/states/credits/CreditsMenu.hx
+++ source/sinco/vsguy/states/credits/CreditsMenu.hx
@@ -5,6 +5,7 @@ import sinco.vsguy.states.credits.json.CreditsList;
 import openfl.Assets;
 import haxe.Json;
 import sinco.vsguy.states.credits.json.*;
+import flixel.FlxG;
 
 class CreditsMenu extends MusicBeatState
 {
@@ -13,18 +14,36 @@ class CreditsMenu extends MusicBeatState
 
     public var textSpeed:Float = 5.0;
 
+	var scrollPaused:Bool = false;
+
+	var pausedText:FlxText;
+	var howToPauseText:FlxText;
+
 	override public function new()
 	{
 		creditsFile = Json.parse(Assets.getText(Paths.json('credits')));
         // trace(credits.credits);
 
         var index:Int = 0;
-        var yOffset:Float = 10.0;
+
+        var num:Int = 0;
+		for (item in creditsFile.credits)
+		{
+			var userList:Array<CreditUser> = item.header.users;
+
+            num++;
+			for (user in userList)
+			{
+				num++;
+			}
+		}
+
+		var yOffset:Float = FlxG.height + num * 30;
         for (item in creditsFile.credits)
         {
 			var userList:Array<CreditUser> = item.header.users;
 
-			var header:FlxText = new FlxText(10, yOffset, 0, item.header.text, 16);
+			var header:FlxText = new FlxText(10, yOffset, 0, item.header.text, 64);
 			yOffset += 30;
 			credits.add(header);
 
@@ -33,7 +52,7 @@ class CreditsMenu extends MusicBeatState
             {
 				var newuser = '${user.person}${user.role.length > 0 ? ' - ${user.role}' : ''}';
 
-				var person:FlxText = new FlxText(header.x, yOffset, 0, newuser, header.size);
+				var person:FlxText = new FlxText(header.x, yOffset, 0, newuser, header.size / 2);
 				yOffset += 30;
 				credits.add(person);
 
@@ -42,6 +61,24 @@ class CreditsMenu extends MusicBeatState
             }
 			index++;
         }
+        
+		pausedText = new FlxText(-10, 10, FlxG.width, 'PAUSED');
+		pausedText.setFormat(Paths.font('vcr.ttf'), 60, FlxColor.WHITE, FlxTextAlign.RIGHT);
+		pausedText.scrollFactor.set(0, 0);
+		pausedText.visible = false;
+
+        /*
+		var upDownText = new FlxText(-10, 670, FlxG.width, 'UP/DOWN to Navigate');
+		upDownText.setFormat(Paths.font('vcr.ttf'), 20, FlxColor.WHITE, FlxTextAlign.RIGHT);
+		upDownText.scrollFactor.set(0, 0);
+		upDownText.alpha = 0.4;
+		add(upDownText);
+        */
+
+		howToPauseText = new FlxText(-10, 695, FlxG.width, 'SPACE/ENTER to Pause');
+		howToPauseText.setFormat(Paths.font('vcr.ttf'), 20, FlxColor.WHITE, FlxTextAlign.RIGHT);
+		howToPauseText.scrollFactor.set(0, 0);
+		howToPauseText.alpha = 0.4;
 
 		super();
 	}
@@ -49,6 +86,8 @@ class CreditsMenu extends MusicBeatState
 	override public function create()
 	{
 		add(credits);
+		add(pausedText);
+		add(howToPauseText);
 
 		super.create();
 	}
@@ -60,10 +99,13 @@ class CreditsMenu extends MusicBeatState
 
         if (controls.ACCEPT)
         {
+			pausedText.visible = false;
             if (textSpeed == 0)
 				FlxTween.tween(this, {textSpeed: 5}, 1.0);
-            else
+            else {
+				pausedText.visible = true;
 				FlxTween.tween(this, {textSpeed: 0}, 1.0);
+            }
         }
 
         for (text in credits)
