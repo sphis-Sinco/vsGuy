700c281366c2d1ce429107bd0f8a2eec232d0083
IdealistCat <mattallan.h@gmail.com>
2024-12-13 17:37:19 -0600

    f3 scene in the works

---
+++ source/sinco/vsguy/substates/f3scene/F3Scene.hx
@@ -0,0 +1,8 @@
+package sinco.vsguy.substates;
+
+import sinco.vsguy.substates.f3scene.components.*;
+
+class F3Scene extends MusicBeatSubstate
+{
+	public var CURRENT_STATE:F3States = GAMEPLAY_STATE;
+}
---
+++ source/sinco/vsguy/substates/f3scene/components/F3States.hx
@@ -0,0 +1,6 @@
+package sinco.vsguy.substates.f3scene.components;
+
+enum abstract F3States(String) from String to String
+{
+	public var GAMEPLAY_STATE:String = 'gameplay';
+}
