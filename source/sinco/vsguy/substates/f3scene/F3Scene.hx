17ff00ef587eaef5b9e49800274b9fb7db771dd8
IdealistCat <mattallan.h@gmail.com>
2024-12-13 17:41:34 -0600

    left text, right text, and function overrides

--- source/sinco/vsguy/substates/f3scene/F3Scene.hx
+++ source/sinco/vsguy/substates/f3scene/F3Scene.hx
@@ -5,4 +5,21 @@ import sinco.vsguy.substates.f3scene.components.*;
 class F3Scene extends MusicBeatSubstate
 {
 	public var CURRENT_STATE:F3States = GAMEPLAY_STATE;
+
+	public var leftText:FlxText;
+	public var rightText:FlxText;
+
+	override public function new()
+	{
+		super();
+	}
+
+	override public function create()
+    {
+        super.create();
+    }
+
+	override public function update(elapsed:Float) {
+        super.update(elapsed);
+    }
 }
