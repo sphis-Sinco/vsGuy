1689d6ac6665180492bb8189b40327af355eefdc
IdealistCat <mattallan.h@gmail.com>
2024-12-15 01:19:40 -0600

    Revert "Funkin SMP"
    This reverts commit e690b185420eb2ec9ae1483868733f58b1d08800.

--- source/sinco/funkin_smp/substates/f3scene/components/F3States.hx
+++
@@ -1,9 +0,0 @@
-package sinco.funkin_smp.substates.f3scene.components;
-
-enum abstract F3States(String) from String to String
-{
-	public var GAMEPLAY_STATE:String = 'gameplay';
-
-	public var MENU_STATE:String = 'menu';
-	public var MAINMENU_STATE:String = 'main-$MENU_STATE';
-}
